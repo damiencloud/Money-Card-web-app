7d6ac6b531daf5d229183c01944e45226b6c6ce9
Damien Joseph Martin <damienjosephmartin10@gmail.com>
2026-08-22 14:17:03 +0530

    fix(flutter): remove pre-filled default email and password from staff login screen

--- Flutter Money card/lib/features/auth/login_screen.dart
+++ Flutter Money card/lib/features/auth/login_screen.dart
@@ -14,8 +14,8 @@ class LoginScreen extends ConsumerStatefulWidget {
 
 class _LoginScreenState extends ConsumerState<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController(text: 'staff@moneycard.io');
-  final _passwordController = TextEditingController(text: 'password123');
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
   bool _obscurePassword = true;
 
   // Standard email validation pattern
